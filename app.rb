1c7634c996184f41c928b33fb530390345eaa921
Larry Price <larry.price.dev@gmail.com>
2013-01-06 11:25:59 -0500

    Removing debug output. Production error fixed by telling Heroku explicitly to use Ruby 1.9.3 (default was 1.9.2).

--- app.rb
+++ app.rb
@@ -5,8 +5,6 @@ require_relative 'pokemon'
 
 class Pokedex < Sinatra::Base
 	configure do
-		puts "This is what's in the current dir:"
-		puts Dir.entries(Dir.pwd)
 	  Mongoid.load! 'mongoid.yml'
 	end
 
