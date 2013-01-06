b008a0416331c58bba72cb9544a7c6f9db274105
Larry Price <larry.price.dev@gmail.com>
2013-01-06 10:43:40 -0500

    Updating populater loop to be significantly simpler.

--- tools/populate/populater.rb
+++ tools/populate/populater.rb
@@ -7,19 +7,13 @@ require 'open-uri'
 class Populater
 	def initialize()
     Pokemon.delete_all
-
-		@data = []
-		load_data
+		@data = Nokogiri::HTML(open("http://pokemon.wikia.com/wiki/List_of_Pok%C3%A9mon"))
 	end
 
-	def load_data
-    @data = Nokogiri::HTML(open("http://pokemon.wikia.com/wiki/List_of_Pok%C3%A9mon"))
-  end
-
-  def add_pokemon(num_pokemon)
-  	num = format_string num_pokemon
-    return if num == "000"
+  def add_pokemon(num_to_add)
+    raise "BadArgumentError" if num_to_add.nil? || !num_to_add.is_a?(Integer)
   	@data.xpath("//table[@class='wikitable sortable']/tr").each do |row|
+      break if num_to_add <= 0
   		dex_num = row.at_xpath('td/text()').to_s.strip
   		next if dex_num.nil? || dex_num.empty?
   		dex_name = row.at_xpath('td[2]/a/text()').to_s.strip
@@ -27,7 +21,7 @@ class Populater
       unless dex_num == "000"
         type_1 = row.at_xpath('td[4]/a/span/text()').to_s.strip
         type_2 = row.at_xpath('td[5]/a/span/text()').to_s.strip || row.at_xpath('td[5]/text()').to_s.strip
-        image_link = "http://img.pokemondb.net/artwork/#{dex_name}.jpg"
+        image_link = "http://img.pokemondb.net/artwork/#{dex_name.downcase}.jpg"
       else
         type_1 = row.at_xpath('td[4]/text()').to_s.strip
         type_2 = row.at_xpath('td[5]/text()').to_s.strip
@@ -38,31 +32,9 @@ class Populater
       types << type_1 unless type_1.nil? || type_1.empty?
       types << type_2 unless type_2.nil? || type_2.empty?
 
-      monster = Pokemon.new :number => dex_num, :name => dex_name, :types => types, :image => image_link
-      monster.save
+      Pokemon.create :number => dex_num, :name => dex_name, :types => types, :image => image_link
 
-      #@pokemon.insert({:number => dex_num, :name => dex_name, :types => types, :image => image_link})
-  		break if dex_num.to_i == num.to_i - 1
+      num_to_add -= 1
   	end
-  end
-
-  def format_string(num)
-    if num.nil? || !num.kind_of?(Integer)
-      raise ArgumentError
-    end
-
-    str_num = num.to_s
-    case str_num.size
-    when 3
-      str_num
-    when 2
-      "0#{str_num}"
-    when 1
-      "00#{str_num}"
-    when 0
-      "000"
-    else
-      "999"
-    end
   end
 end
--- tools/test/spec/populater_spec.rb
+++ tools/test/spec/populater_spec.rb
@@ -12,51 +12,19 @@ describe Populater do
 	end
 
 	describe "#new" do
+		it "does not throw when creating instance" do
+			expect {Populater.new}.to_not raise_error
+		end
+
 		it "takes no params and returns a Populater instance" do
 			@populater.should be_an_instance_of Populater
 		end
 
 		it "empties pokemon collection" do
-			Pokemon.create!
+			Pokemon.create
 			Pokemon.count.should eql 1
 			Populater.new
 			Pokemon.count.should eql 0
-		end
-	end
-
-	describe "#format_string" do
-		it "takes a single integer parameter" do
-			expect {@populater.format_string 1}.to_not raise_error
-		end
-
-		it "raises error if param is nil" do
-			expect {@populater.format_string nil}.to raise_error
-		end
-
-		it "raises error if param is not integer" do
-			expect {@populater.format_string "500"}.to raise_error
-		end
-
-		it "returns 000 for 0" do
-			@populater.format_string(0).should eql "000"
-		end
-
-		it "returns number padded with zeros for single-digit number" do
-			(0..9).each {|x| @populater.format_string(x).should eql "00#{x}"}
-		end
-
-		it "returns number padded with zero for two-digit number" do
-			x = 35
-			@populater.format_string(x).should eql "0#{x}"
-		end
-
-		it "returns number as string for three-digit number" do
-			x = 456
-			@populater.format_string(x).should eql "#{x}"
-		end
-
-		it "returns 999 for num with more than three digits" do
-			@populater.format_string(3500).should eql "999"
 		end
 	end
 
